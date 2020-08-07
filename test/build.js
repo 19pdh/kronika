5bd7f8441402ef21a11c97edf66f90dae2e59592
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-08-07 23:13:03 +0200

    Add feed options

--- test/build.js
+++ test/build.js
@@ -3,4 +3,4 @@ import Kronikarz from 'kronikarz';
 const k = new Kronikarz('../wpisy');
 
 k.generateApi('../static');
-k.generateRss('../static/rss.xml');
+k.generateRss('../static/rss.xml', {title: "Kronika 19PDH Puszcza", feed_url: "https://puszcza.netlify.app/rss.xml", site_url: "https://puszcza.netlify.app"} );
