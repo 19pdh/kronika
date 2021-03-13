f630d296af067bdfa5ad9fa423d36aa629f17148
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2021-03-13 20:39:27 +0100

    Remove unnecessary files

--- test/build.js
+++
@@ -1,6 +0,0 @@
-import Kronikarz from 'kronikarz';
-
-const k = new Kronikarz('../wpisy');
-
-k.generateApi('../static');
-k.generateRss('../static/rss.xml', {title: "Kronika 19PDH Puszcza", feed_url: "https://puszcza.netlify.app/rss.xml", site_url: "https://puszcza.netlify.app"} );
--- test/validator.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-NEW_FILES=`git diff --name-only HEAD master | egrep 'wpisy/*'`
-
-for file in $NEW_FILES; do
-    echo "../$file" > path.in
-
-    node -r esm check.js < path.in
-done
